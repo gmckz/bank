ff4fa5fc2edb5bd1e8a18271ba07014135e2eebd
gmckz <gemma_mckenzie@hotmail.co.uk>
2023-09-05 18:16:47 +0100

    removed comments

--- account.js
+++ account.js
@@ -42,16 +42,13 @@ class Account {
 	}
 
 	printStatement() {
-		// set the statement header
 		const statementHeader = "date || credit || debit || balance";
 		let statement = statementHeader;
-		// return statement header if no transactions
 		if (!this.transactions.length) {
 			return statement;
 		}
-		//reverse order of transactions array
+
 		const statementTransactions = this.transactions.toReversed();
-		// create string for each transaction and push to statement
 		statementTransactions.forEach((transaction) => {
 			if (transaction.credit === null) {
 				const stringTransaction = `\n${transaction.date} || || ${transaction.debit} || ${transaction.balance}`;
